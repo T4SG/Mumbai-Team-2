ec4c60a2676104ad60bd252bf98cbc0ca041f7ee
duckvader <shahdakshil@gmail.com>
2015-07-11 19:16:22 +0530

    login

--- login-system/database-config.php
+++ login-system/database-config.php
@@ -1,9 +1,9 @@
 <?php
    // define database related variables
-   $database = 'techyari_demos';
-   $host = 'localhost';
-   $user = 'root';
-   $pass = '';
+   $database = 'db name';
+   $host = 'location of db';
+   $user = 'db user id';
+   $pass = 'db login password';
 
    // try to conncet to database
    $dbh = new PDO("mysql:dbname={$database};host={$host};port={3306}", $user, $pass);
