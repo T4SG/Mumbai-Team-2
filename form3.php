ce28ce26c346f1dc5f0f18b4d01a57c99588c970
duckvader <shahdakshil@gmail.com>
2015-07-12 05:17:50 +0530

    all php

---
+++ form3.php
@@ -0,0 +1,171 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Happy Hearts Fund</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="css/main.css" />
+		<link rel="stylesheet" href="css/carousel.css" />
+		<script type="text/javascript" src="js/jquery.js"></script>
+    	<script type="text/javascript" src="js/jquery.min.js"></script>
+    	<script type="text/javascript" src="js/bootstrap.min.js"></script>     
+    	<script type="text/javascript" src="js/bootstrap.js"></script>     
+    	<script type="text/javascript">
+function validateForm() {
+    if( document.myForm.inc_incr.value == "" || isNaN(document.myForm.inc_incr.value) )
+         {
+            alert( "Please provide increase in average household income" );
+            document.myForm.inc_incr.focus() ;
+            return false;
+         }
+    if( document.myForm.inc_jobs.value == "" || isNaN(document.myForm.inc_jobs.value) )
+         {
+            alert( "Please provide increase in number of jobs" );
+            document.myForm.inc_jobs.focus() ;
+            return false;
+         }
+         
+    if( document.myForm.n_shelter.value == "" || isNaN(document.myForm.n_shelter.value))
+         {
+            alert( "Please provide number of people sheltering in school" );
+            document.myForm.n_shelter.focus() ;
+            return false;
+         }  
+         if( document.myForm.comm_hours.value == "" || isNaN(document.myForm.comm_hours.value) )
+         {
+            alert( "Please provide Volunteer hours" );
+            document.myForm.comm_hours.focus() ;
+            return false;
+         }
+      return true;
+	}	
+	</script>	
+
+	</head>
+	<body>
+		<div id="page-wrapper">
+				<div id="header">
+						<h1><a href="index.html" id="logo"><em>Happy Hearts Fund</em></a></h1>
+						<nav id="nav">
+							<ul>
+								<li class="current"><a href="form3.html">Home</a></li>
+								<li>
+									<a href="#">Dropdown</a>
+									<ul>
+										<li><a href="login.html">login</a></li>
+										<li><a href="dashboards.html">dashboard</a></li>
+										<li><a href="form1.html">form1</a></li>
+										<li><a href="form2.html">form2</a></li>
+										<li><a href="form3.html">form3</a></li>
+										<li><a href="aboutus.html">about us</a></li>
+									</ul>
+								</li>
+							</ul>
+						</nav>
+
+
+
+				</div>
+				
+				<div align='centre'><!-- form3 --><br>
+					
+<form style= "margin: 0 auto; width:20%" name="myForm" action="demo_form.asp" onsubmit="return (validateForm());" method="post">
+    <h2>Economic Metrics</h2>
+	
+	
+	Increase in average household income: <input type="text" name="inc_incr" placeholder="e.g. 20000" required></input>
+	<br>
+	Increase in number of jobs in area: <input type="text" name="inc_jobs" placeholder="e.g. 450"/ required><br>
+	Number of people sheltering in school:<input type="text" name="n_shelter" placeholder="e.g. 550" required></input><br>
+	Volunteer hours for community service:<input type="text" name="comm_hours" placeholder="e.g. 2000" required></input><br>
+
+<center><input type="submit" value="Submit Form"></input></center>
+</form>
+				</div>
+				
+			<!-- Banner -->
+				<section id="banner">
+					<header>
+						<h2>Happy Hearts Fund</h2>
+						<a href="#" class="button">Learn More</a>
+					</header>
+				</section>
+
+			
+
+			<!-- Footer -->
+				<div id="footer">
+					<div class="container">
+						<div class="row">
+							<section class="3u 6u(narrower) 12u$(mobilep)">
+								<h3>Links to Stuff</h3>
+								<ul class="links">
+									<li><a href="#">link1</a></li>
+									<li><a href="#">link2</a></li>
+									
+								</ul>
+							</section>
+							<section class="3u 6u$(narrower) 12u$(mobilep)">
+								<h3>More Links to Stuff</h3>
+								<ul class="links">
+									<li><a href="#">link3</a></li>
+									<li><a href="#">link4</a></li>
+									
+								</ul>
+							</section>
+							<section class="6u 12u(narrower)">
+								<h3>Get In Touch</h3>
+								<form>
+									<div class="row 50%">
+										<div class="6u 12u(mobilep)">
+											<input type="text" name="name" id="name" placeholder="Name" />
+										</div>
+										<div class="6u 12u(mobilep)">
+											<input type="email" name="email" id="email" placeholder="Email" />
+										</div>
+									</div>
+									<div class="row 50%">
+										<div class="12u">
+											<textarea name="message" id="message" placeholder="Message" rows="5"></textarea>
+										</div>
+									</div>
+									<div class="row 50%">
+										<div class="12u">
+											<ul class="actions">
+												<li><input type="submit" class="button alt" value="Send Message" /></li>
+											</ul>
+										</div>
+									</div>
+								</form>
+							</section>
+						</div>
+					</div>
+
+					<!-- Icons -->
+						<ul class="icons">
+							<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+							<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+							<li><a href="#" class="icon fa-linkedin"><span class="label">LinkedIn</span></a></li>
+							<li><a href="#" class="icon fa-google-plus"><span class="label">Google+</span></a></li>
+						</ul>
+
+					<!-- Copyright -->
+						<div class="copyright">
+							<ul class="menu">
+								<li>&copy; Happy Hearts Fund. All rights reserved</li>
+							</ul>
+						</div>
+
+				</div>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="js/jquery.min.js"></script>
+			<script src="js/jquery.dropotron.min.js"></script>
+			<script src="js/skel.min.js"></script>
+			<script src="js/util.js"></script>
+			<script src="js/main.js"></script>
+
+	</body>
+</html>
---
+++ pie.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+	<head>
+		<title>Pie Chart</title>
+		<script src="js/Chart.js"></script>
+		<script src="js/piegrapher.js"></script>
+	</head>
+	<body>
+		<div id="canvas-holder">
+			<canvas id="chart-area" width="300" height="300"/>
+		</div>
+	</body>
+</html>
