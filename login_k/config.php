83ae38f93c7de8b0d4abb130431591000c88fe39
Nilay Shah <nilayspshah@gmail.com>
2015-07-12 01:17:48 +0530

    working on amazon aws

--- login_k/config.php
+++ login_k/config.php
@@ -1,8 +1,8 @@
 <?php
 	define('DB_HOST', 'localhost');
     define('DB_USER', 'root');
-    define('DB_PASSWORD', 'cfg');
-    define('DB_DATABASE', 'cfg');
+    define('DB_PASSWORD', 'code4good');
+    define('DB_DATABASE', 'happy_hearts');
   
 
 ?>
