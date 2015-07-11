40385a7d4c300592f42d0ff6fbebd1a9700704aa
duckvader <shahdakshil@gmail.com>
2015-07-12 02:21:33 +0530

    ya

---
+++ js/graph_fetch_pie_data.js
@@ -0,0 +1,7 @@
+var school_id = <?php echo $school_id ?>; // no
+var date_update = "<?php echo $date_update ?>"; // (no as a string)
+var teacher = <?php echo $teacher ?>;
+var boys = <?php echo $boys ?>;
+var girls = <?php echo $girls ?>;
+var new_enroll = <?php echo $new_enroll ?>;
+var no_left = <?php echo $no_left ?>;
